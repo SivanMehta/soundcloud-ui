3579ce9dcfb601d19a65273677b8a3d12208a603
Sivan Mehta <Sivanmehta2@gmail.com>
2019-04-27 21:54:32 -0700

    double size

--- graph.js
+++ graph.js
@@ -1,27 +1,22 @@
-const n = 100;
+const n = 200;
 const width = 4;
 
 const bars = Array(n).fill(0)
-  .map((_, i) => ({
-    i,
-    height: Math.round(Math.random() * 20 + 100)
-  }));
-
+  .map(() => Math.round(Math.random() * 20 + 100));
 
 // setup rectangles
 d3.select('#content').selectAll('rect')
   .data(bars).enter()
   .append('rect')
-  .attr('x', d => (n * width) - d.i * width)
-  .attr('y', d => 140 - d.height)
+  .attr('x', (d, i) => (n * width) - i * width)
+  .attr('y', d => 140 - d)
   .attr('width', width)
-  .attr('height', d => d.height)
+  .attr('height', d => d)
   .attr('class', 'future')
-  .attr('id', d => `rect-${d.i}`)
-
+  .attr('id', (d, i) => `rect-${i}`);
 
 let active = 0;
 setInterval(() => {
   d3.select(`#rect-${n - active}`).attr('class', 'past');
   active += 1;
-}, 500);
+}, 100);
